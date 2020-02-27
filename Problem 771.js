00f19d759d35b4a70463f64ec0f764c06b40be46
Nahin Juma Edmonds <38460249+ninjanahin@users.noreply.github.com>
2020-02-28 05:02:37 +0800

    Update Problem 771 Solution
    [Update]
    Reason: Improved speed efficiency. Essentially using the same idea, just removing unnecessary extra function calls.
    Runtime: 56ms

--- Problem 771.js
+++ Problem 771.js
@@ -4,31 +4,21 @@
  * @return {number}
  */
 var numJewelsInStones = function(J, S) {
-    var dict = {}; //Dictionary we will use for counting (Key/No. of seen)
+    var dict = {}; //Dictionary we will use for key checking
     counter = 0; //Counter for determining no. of stones that are jewels
     
-    //Cycle through and count the number of each character
+    //Create a dictionary key for each Jewel (so we can use 'in')
+    for(i=0; i< J.length; i++)
+    {
+        dict[J[i]] = 0;
+    }
+    
     for(i=0; i< S.length; i++)
     {
         if(S[i] in dict)
         {
-            //If the key exists in the dictionary add 1 to the number seen
-            dict[S[i]] += 1; //Increment the counter by 1
-        }
-        else
-        {
-            //The key doesn't exist, create key with number seen at 1
-            dict[S[i]] = 1;
-        }
-    }
-    
-    for(i=0; i< J.length; i++)
-    {
-        if(J[i] in dict)
-        {
-            //If the key exists in the dictionary
-            //then add the no. of stones that are jewels 
-            counter += dict[J[i]]
+            //If the stone matches the Jewel key, increase the global counter by 1
+            counter ++;
         }
     }
     
